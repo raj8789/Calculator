53787d228e25d97855d089b24a173b12043dfca2
raushan raj <raushan.raj@khoros.com>
2021-10-20 11:02:09 +0530

    Added after adding c variable

--- src/Sum.java
+++ src/Sum.java
@@ -4,6 +4,8 @@ public class Sum {
     public static void main(String[] args) {
         int a=10;
         int b=20;
+        int c=30;
         System.out.println(a+b);
+        System.out.println(a+b+c);
     }
 }
